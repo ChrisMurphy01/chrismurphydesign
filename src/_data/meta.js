b7c96e1fb12df3161e4b6aca06cd20e1b8938898
Chris Murphy <c.murphy@trustedhousesitters.com>
2021-08-27 11:17:39 +0100

    update title & desc

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "Chris Murphy Design Studio",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed, and social share preview images.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+    "Furniture and lighting. Unique designs, crafted to the highest standards. Handmade in Hove, England.",
+  authorName: "Chris Murphy",
+  twitterUsername: "cmurphy_design", // no `@`
 };
